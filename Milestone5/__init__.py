a30c480294a3a568b913bb87a90d136a7269161e
Hii.Nian.Yu <wqd180038@siswa.um.edu.my>
2020-06-19 22:12:08 +0800

    change init.py

--- Milestone5/__init__.py
+++ Milestone5/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import pandas as pd
-from gp.Milestone5 import getRegressionFunction
+import numpy as np
+from math import ceil
+from sklearn.linear_model import LinearRegression
 
 ALLOWED_EXTENSIONS = {'csv'}
 
@@ -8,6 +10,39 @@ app = Flask(__name__)
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def getRegressionFunction(dataFrame):
+    Df = dataFrame
+    
+    Df["Date"] = pd.to_datetime(Df["Date"]).dt.strftime('%Y-%m-%d')
+    Df['Price']=Df['Price'].astype(str).str.replace(',', '').astype(float)
+    Df['S_1'] = Df["Price"].shift(1).rolling(window=2, center = True).mean()
+    Df['S_2'] = Df["Price"].shift(1).rolling(window=5, center = True).mean()
+    Df = Df.dropna()
+    X = Df[['S_1','S_2']]
+    y = Df["Price"]
+    
+    t = 0.9
+    count = int(ceil(X.shape[0]*t))
+    X = X.iloc[::-1]
+    y = y.iloc[::-1]
+    X_train = X[:count]
+    X_test = X[count:]
+    y_train = y[:count]
+    y_test = y[count:]
+    # Performing linear regression
+    linear = LinearRegression().fit(X_train, y_train)
+    result = Coefficient(round(linear.coef_[0], 2), round(linear.coef_[1], 2), round(linear.intercept_, 2))
+    # Predict prices
+    predicted_price = linear.predict(X_test)
+    predicted_price = pd.DataFrame(predicted_price, index=y_test.index, columns=['price'])
+    return result
+
+class Coefficient:
+    def __init__(self, M1, M2, C):
+        self.M1 = M1
+        self.M2 = M2
+        self.C = C
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
